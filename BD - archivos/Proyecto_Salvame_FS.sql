854073d44f90efc3adc44cb8357087d6a516bc25
Kevin Pineda <pinedasilupukevin.20@gmail.com>
2023-10-31 21:05:40 -0500

    BD Detallada

--- BD - archivos/Proyecto_Salvame_FS.sql
+++ BD - archivos/Proyecto_Salvame_FS.sql
@@ -8,14 +8,27 @@ LOGGING;
 -- Eliminar Table space
 --DROP TABLESPACE PROYECTOSALVAME INCLUDING CONTENTS AND DATAFILES;
 
---Creacion de Usuario adminSalvame: contiene todos los permisos de la base de datos
-CREATE USER C##admin_salvame IDENTIFIED BY admin_salvame;
-GRANT ALL PRIVILEGES TO C##admin_salvame;
+--Creacion de Usuario sistemaSalvame: contiene todos los permisos de la base de datos
 
-CREATE USER C##sistema_salvame IDENTIFIED BY sistema_salvame;
+
+CREATE USER C##sistema_Salvame IDENTIFIED BY sistema_salvame;
 GRANT ALL PRIVILEGES TO C##sistema_salvame;
 
-CREATE TABLE C##sistema_salvamee.usuario_basico (
+--probando
+--CREATE TABLE historial_alerta (
+  --idHistorialA VARCHAR2(30) PRIMARY KEY,
+  --estatus VARCHAR2(20) NOT NULL
+--) TABLESPACE PROYECTOSALVAME;
+
+--Se debe crear las tablas en el siguiente orden
+
+CREATE TABLE C##sistema_salvame.perfil_usuario (
+  idPerfilUsuario CHAR(10) PRIMARY KEY,
+  rol VARCHAR2(30) NOT NULL,
+  descripcion VARCHAR2(200)  
+) TABLESPACE PROYECTOSALVAME;
+
+CREATE TABLE C##sistema_salvame.usuario_basico (
   nroDocumento CHAR(10) PRIMARY KEY,
   idPerfilUsuario CHAR(10),
   FOREIGN KEY (idPerfilUsuario) REFERENCES C##sistema_salvame.perfil_usuario(idPerfilUsuario),
@@ -24,14 +37,6 @@ CREATE TABLE C##sistema_salvamee.usuario_basico (
   correo VARCHAR2(50),
   fechaNac DATE NOT NULL
 ) TABLESPACE PROYECTOSALVAME;
-
-CREATE TABLE C##admin_salvame.perfil_usuario (
-  idPerfilUsuario CHAR(10) PRIMARY KEY,
-  rol VARCHAR2(30) NOT NULL,
-  descripcion VARCHAR2(200)  
-) TABLESPACE PROYECTOSALVAME;
-
---DROP TABLE  C##admin_salvame.perfil_usuario;
 
 CREATE TABLE C##sistema_salvame.usuario_especial (
   nroDocumento CHAR(10) PRIMARY KEY,
@@ -51,29 +56,6 @@ CREATE TABLE C##sistema_salvame.usuario_especial (
   imgCertiONG  BLOB
 ) TABLESPACE PROYECTOSALVAME;
 
-CREATE TABLE C##sistema_salvame.alerta (
-  idAlerta CHAR(10) PRIMARY KEY,
-  nroDoc CHAR(10) NOT NULL,
-  codigoPostal VARCHAR2(30) NOT NULL,
-  idHistorialA VARCHAR2(30) NOT NULL,
-  idAnimal VARCHAR2(30),
-  fechaAlerta VARCHAR2(30) NOT NULL,
-  horaAlerta VARCHAR2(30) NOT NULL,
-  tipoAlerta VARCHAR2(30) NOT NULL,
-  descripcionAnimal VARCHAR2(30),
-  descripcionHechos VARCHAR2(30),
-  imgEvidencia VARCHAR2(50) NOT NULL,
-  FOREIGN KEY (nroDoc) REFERENCES C##sistema_salvame.usuario_basico(nroDoc),
-  FOREIGN KEY (codigoPostal) REFERENCES C##sistema_salvame.ubicacion(codigoPostal),
-  FOREIGN KEY (idHistorialA) REFERENCES C##sistema_salvame.historial_alerta(idHistorialA),
-  FOREIGN KEY (idAnimal) REFERENCES C##sistema_salvame.animal(idAnimal)
-) TABLESPACE PROYECTOSALVAME;
-
-CREATE TABLE C##sistema_salvame.historial_alerta (
-  idHistorialA VARCHAR2(30) PRIMARY KEY,
-  estatus VARCHAR2(20) NOT NULL
-) TABLESPACE PROYECTOSALVAME;
-
 CREATE TABLE C##sistema_salvame.animal (
   idAnimal VARCHAR2(30) PRIMARY KEY,
   nombreCientifico VARCHAR2(30),
@@ -81,12 +63,7 @@ CREATE TABLE C##sistema_salvame.animal (
   familia VARCHAR2(30) NOT NULL,
   especie VARCHAR2(30) NOT NULL,
   genero VARCHAR2(20),
-  descripcion VARCHAR2(200) NOT NULL,
-) TABLESPACE PROYECTOSALVAME;
-
-CREATE TABLE C##sistema_salvame.historial_alerta (
-  idHistorialA VARCHAR2(30) PRIMARY KEY,
-  estatus VARCHAR2(20) NOT NULL
+  descripcion VARCHAR2(200) NOT NULL
 ) TABLESPACE PROYECTOSALVAME;
 
 CREATE TABLE C##sistema_salvame.ubicacion (
@@ -96,5 +73,31 @@ CREATE TABLE C##sistema_salvame.ubicacion (
   altitud NUMBER NOT NULL,
   direccion VARCHAR2(30) NOT NULL,
   provincia VARCHAR2(30),
-  departamento VARCHAR2(30),
+  departamento VARCHAR2(30)
 ) TABLESPACE PROYECTOSALVAME;
+
+CREATE TABLE C##sistema_salvame.historial_alerta (
+  idHistorialA VARCHAR2(30) PRIMARY KEY,
+  estatus VARCHAR2(20) NOT NULL
+) TABLESPACE PROYECTOSALVAME;
+
+CREATE TABLE C##sistema_salvame.alerta (
+  idAlerta CHAR(10) PRIMARY KEY,
+  nroDocumento CHAR(10) NOT NULL,
+  codigoPostal VARCHAR2(30) NOT NULL,
+  idHistorialA VARCHAR2(30) NOT NULL,
+  idAnimal VARCHAR2(30),
+  fechaAlerta VARCHAR2(30) NOT NULL,
+  horaAlerta VARCHAR2(30) NOT NULL,
+  tipoAlerta VARCHAR2(30) NOT NULL,
+  descripcionAnimal VARCHAR2(30),
+  descripcionHechos VARCHAR2(30),
+  imgEvidencia VARCHAR2(50) NOT NULL,
+  FOREIGN KEY (nroDocumento) REFERENCES C##sistema_salvame.usuario_basico(nroDocumento),
+  FOREIGN KEY (codigoPostal) REFERENCES C##sistema_salvame.ubicacion(codigoPostal),
+  FOREIGN KEY (idHistorialA) REFERENCES C##sistema_salvame.historial_alerta(idHistorialA),
+  FOREIGN KEY (idAnimal) REFERENCES C##sistema_salvame.animal(idAnimal)
+) TABLESPACE PROYECTOSALVAME;
+
+
+
